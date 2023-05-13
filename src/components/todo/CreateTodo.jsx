1edc2d6d866dd432d698c9ef51d3e93321e365e3
Jeongeum <wjddma1202@naver.com>
2023-05-14 02:04:08 +0900

    fix : 투두리스트 등록 기능 함수이동 및 오류 수정중 #14

--- src/components/todo/CreateTodo.jsx
+++ src/components/todo/CreateTodo.jsx
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../common/Input/Input";
+import { CreateTodoDiv } from "./styled";
+import CreateButton from "../common/Button/CreateButton";
+import DeleteButton from "../common/Button/DeleteButton";
 
-export const CreateTodo = ({ inputValue, onChangeInput, onClickAddTodo }) => {
+export const CreateTodo = ({
+  hidden,
+  setTodoData,
+  inputValue,
+  setInputValue,
+  onClickHidden,
+  onChangeInput,
+}) => {
+  // 등록 버튼 클릭
+  const onClickAddTodo = (e) => {
+    if (inputValue && e.key === "Enter") {
+      setTodoData((prev) => [
+        ...prev,
+        {
+          id: Date.now(),
+          content: inputValue,
+          isEdit: false,
+          isDone: false,
+        },
+      ]);
+      setInputValue("");
+    }
+  };
   return (
-    <>
-      <Input
-        placeholder="할일 등록"
-        value={inputValue}
-        onChange={onChangeInput}
-      />
-      <button type="submit" onClick={onClickAddTodo}>
-        +
-      </button>
-    </>
+    <CreateTodoDiv>
+      {hidden === false ? (
+        <>
+          <Input
+            placeholder="할일 등록"
+            value={inputValue}
+            onChange={onChangeInput}
+            onKeyDown={(e) => onClickAddTodo(e)}
+          />
+          <DeleteButton onClick={onClickHidden}>+</DeleteButton>
+        </>
+      ) : (
+        <CreateButton type="submit" onClick={onClickHidden}>
+          +
+        </CreateButton>
+      )}
+    </CreateTodoDiv>
   );
 };
