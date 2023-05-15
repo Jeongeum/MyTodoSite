0c053b03571bdef91f6c71db0df39be935935fb5
Jeongeum <wjddma1202@naver.com>
2023-05-15 16:47:44 +0900

    feat : 로컬스토리지 데이터 불러오지 못했던 오류에 대한 방법 추가

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -4,10 +4,14 @@ import { TodoList } from "./TodoList";
 import { CreateTodo } from "./CreateTodo";
 
 export const Todo = () => {
+  // useState 초기값이 함수일 경우
   const [todoData, setTodoData] = useState(() => {
     const localData = localStorage.getItem("mytodolist");
     return localData ? JSON.parse(localData) : [];
   });
+
+  // // useState 초기값이 배열일 경우
+  // const [todoData, setTodoData] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [hidden, setHidden] = useState(true);
 
@@ -21,10 +25,26 @@ export const Todo = () => {
     setInputValue(e.target.value);
   };
 
+  // useState 초기값이 함수일 경우
   // 등록 후, data 변화가 생길 때 마다 로컬스토리지에 저장
   useEffect(() => {
     localStorage.setItem("mytodolist", JSON.stringify(todoData));
   }, [todoData]);
+
+  // // useState 초기값이 배열일 경우
+  // // 초기 렌더링 시, 로컬스토리지의 데이터를 가져와 todoData에 넣는다.
+  // useEffect(() => {
+  //   const localData = localStorage.getItem("mytodolist");
+
+  //   if (localData) {
+  //     setTodoData(JSON.parse(localData));
+  //   }
+  // }, []);
+
+  // // todoData에 변화가 생길 때 마다 로컬스토리지에 저장
+  // useEffect(() => {
+  //   localStorage.setItem("mytodolist", JSON.stringify(todoData));
+  // }, [todoData]);
 
   return (
     <TodoListWrapper>
