4bdb1118828ae3a2025c4e833bc05c1cfe8b6a54
Jeongeum <wjddma1202@naver.com>
2023-05-17 18:04:14 +0900

    feat : 닉네임, 패스워드 입력 안했을 경우 버튼 비활성화되도록 구현 #23

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { LoginBtn, LoginWrapper } from "./styled";
 import Img from "../../components/common/Img/Img";
 import Input from "../../components/common/Input/Input";
@@ -6,16 +6,43 @@ import LogoIcon from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
+  const [loginValue, setLoginValue] = useState({
+    usernickname: "",
+    userpassword: "",
+  });
+  const [nickname, setNickname] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <LoginWrapper>
-      <Img src={LogoIcon} width="178px" height="65px" />
-      <p>내 브라우저에서 관리하는 오늘의 일정</p>
+      <form>
+        <Img src={LogoIcon} width="178px" height="65px" />
+        <p>내 브라우저에서 관리하는 오늘의 일정</p>
 
-      <Input placeholder="👤 닉네임을 입력해주세요" />
-      <Input placeholder="🔒 비밀번호를 입력해주세요" />
-      <Link to="/home">
-        <LoginBtn>로그인</LoginBtn>
-      </Link>
+        <Input
+          placeholder="👤 닉네임을 입력해주세요"
+          onChange={(e) => onChangeNickname(e)}
+          value={nickname}
+        />
+        <Input
+          placeholder="🔒 비밀번호를 입력해주세요"
+          onChange={(e) => onChangePassword(e)}
+          value={password}
+        />
+        <Link to="/home">
+          <LoginBtn type="submit" disabled={!nickname || !password}>
+            로그인
+          </LoginBtn>
+        </Link>
+      </form>
     </LoginWrapper>
   );
 };
