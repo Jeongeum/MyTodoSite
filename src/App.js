560c7c5cc6b547878300fc04e7fb18c8cd14aa0a
Jeongeum <wjddma1202@naver.com>
2023-05-18 23:58:23 +0900

    feat : 회원가입 및 로그인 기능 구현중 #23

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
 import Globalstyle from "./styles/Globalstyle";
+import { Signup } from "./pages/Signup/Signup";
 
 function App() {
   return (
     <>
       <Globalstyle />
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </>
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,21 +1,22 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../../firebase";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  updateProfile,
-} from "firebase/auth";
 import { LoginBtn, LoginWrapper } from "./styled";
 import Img from "../../components/common/Img/Img";
-import Input from "../../components/common/Input/Input";
 import LogoIcon from "../../assets/images/logo.png";
-import { useNavigate } from "react-router-dom";
+import Input from "../../components/common/Input/Input";
 
-export const Login = () => {
+export const Signup = () => {
+  const [nickname, setNickname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
 
   const onChangeEmail = (e) => {
     setEmail(e.target.value);
@@ -25,14 +26,24 @@ export const Login = () => {
     setPassword(e.target.value);
   };
 
-  // 로그인 함수
+  // 회원가입 및 로그인 함수
   const onSubmitLoginInfo = async (e) => {
     e.preventDefault();
     try {
-      // Firebase에 해당 이메일과 비밀번호로 사용자가 있는지 확인
-      await signInWithEmailAndPassword(auth, email, password);
-      // 로그인 처리가 완료되면 홈 페이지로 이동
-      navigate("/home");
+      // Firebase에 회원가입 정보를 등록
+      const createdUser = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      // 생성된 사용자의 UID를 활용하여 nickname을 Firestore에 저장
+      await updateProfile(createdUser.user, {
+        displayName: nickname,
+      });
+
+      // 회원가입이 완료되면 로그인 화면으로 이동
+      navigate("/login");
     } catch (error) {
       setError(error.message);
     }
@@ -51,13 +62,21 @@ export const Login = () => {
           onChange={(e) => onChangeEmail(e)}
         />
         <Input
+          placeholder="👤 닉네임을 입력해주세요"
+          onChange={(e) => onChangeNickname(e)}
+          value={nickname}
+        />
+        <Input
           placeholder="🔒 비밀번호를 입력해주세요"
           onChange={(e) => onChangePassword(e)}
           value={password}
         />
         <LoginBtn type="submit" disabled={!email || !password}>
-          로그인
+          회원가입
         </LoginBtn>
+        <Link to="/login">
+          <LoginBtn type="button">로그인 화면으로 이동</LoginBtn>
+        </Link>
       </form>
     </LoginWrapper>
   );
