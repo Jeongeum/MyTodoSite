bb8f6f6aa800e407014fd91782c9a1db03a679c8
Jeongeum <wjddma1202@naver.com>
2023-05-23 17:41:41 +0900

    design : hover시 이미지 바뀌도록 구현

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { Text, TodoItemBtn, TodoItemCheckBtn, TodoItemLi } from "./styled";
 import Img from "../common/Img/Img";
-import EditIcon from "../../assets/images/pencilColor.png";
-import DelteIcon from "../../assets/images/deleteColor.png";
+import EditIcon from "../../assets/images/pencil.png";
+import DeleteIcon from "../../assets/images/delete.png";
+import EditColorIcon from "../../assets/images/pencilColor.png";
+import DeleteColorIcon from "../../assets/images/deleteColor.png";
 import CheckIcon from "../../assets/images/check.png";
 
 export const TodoItem = ({
@@ -15,6 +17,8 @@ export const TodoItem = ({
 }) => {
   const { id } = item;
   const [newText, setNewText] = useState(text);
+  const [editHover, setEditHover] = useState(false);
+  const [deleteHover, setDeleteHover] = useState(false);
 
   // 수정
   const onClickEdit = (id) => {
@@ -81,12 +85,28 @@ export const TodoItem = ({
       </div>
       <TodoItemBtn>
         {isEdit ? null : (
-          <button onClick={() => onClickEdit(id)}>
-            <Img src={EditIcon} width="20px"></Img>
+          <button
+            onClick={() => onClickEdit(id)}
+            onMouseOver={() => setEditHover(true)}
+            onMouseOut={() => setEditHover(false)}
+          >
+            <Img
+              id="editicon"
+              src={editHover ? EditColorIcon : EditIcon}
+              width="20px"
+            ></Img>
           </button>
         )}
-        <button onClick={() => onClickDelete(id)}>
-          <Img src={DelteIcon} width="20px"></Img>
+        <button
+          onClick={() => onClickDelete(id)}
+          onMouseOver={() => setDeleteHover(true)}
+          onMouseOut={() => setDeleteHover(false)}
+        >
+          <Img
+            id="deleteicon"
+            src={deleteHover ? DeleteColorIcon : DeleteIcon}
+            width="20px"
+          ></Img>
         </button>
       </TodoItemBtn>
     </TodoItemLi>
