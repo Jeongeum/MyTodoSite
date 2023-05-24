e8caecb5b0a268348e2d3d84af0a5ae07797acac
Jeongeum <wjddma1202@naver.com>
2023-05-24 15:40:11 +0900

    feat : 다크모드 기능 구현2 #38

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import Home from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
 import Globalstyle from "./styles/Globalstyle";
 import { Signup } from "./pages/Signup/Signup";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./styles/theme";
 
 function App() {
-  const [themeMode, setThemeMode] = useState("lightTheme");
+  const localThemeMode = window.localStorage.getItem("theme" || "lightTheme");
+  const [themeMode, setThemeMode] = useState(localThemeMode);
   const theme = themeMode === "lightTheme" ? lightTheme : darkTheme;
 
-  const toggleTheme = () => {
-    setThemeMode(themeMode === "lightTheme" ? "darkTheme" : "lightTheme");
-  };
+  const toggleTheme = useCallback(() => {
+    if (themeMode === "lightTheme") {
+      setThemeMode("darkTheme");
+      window.localStorage.setItem("theme", "darkTheme");
+    } else {
+      setThemeMode("lightTheme");
+      window.localStorage.setItem("theme", "lightTheme");
+    }
+  });
   return (
     <>
       <ThemeProvider theme={theme}>
         <Globalstyle />
         <Routes>
-          <Route path="/home" element={<Home toggleTheme={toggleTheme} />} />
+          <Route
+            path="/home"
+            element={<Home toggleTheme={toggleTheme} themeMode={themeMode} />}
+          />
           <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<Login />} />
         </Routes>
