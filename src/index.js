c7ef316ea9cd2bbf181a0707aed753a280a83d24
Jeongeum <wjddma1202@naver.com>
2023-05-29 13:20:04 +0900

    fix : FLoC 제한 관련 오류 해결을 위한 경로 수정 (HashRouter)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
